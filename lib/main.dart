68744619a71d81b283bd79c03fe706a215a1f543
skyminseo <skymin0516@gmail.com>
2024-05-28 14:28:37 +0800

    Set up firebase for login fuction

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:wander_wise/components/text_theme.dart';
 import 'package:wander_wise/screen/home_screen.dart';
 import 'package:wander_wise/screen/start_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
